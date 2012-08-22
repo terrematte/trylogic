bb09992be0c776c695e78eb4f8c539e5caf6db0c
terrematte <git@github.com:terrematte/Logicamente.git>
2012-08-22 20:38:25 -0300

    Add first lessons of Natural Deduction for Coq

--- Makefile
+++ Makefile
@@ -5,17 +5,21 @@ all:
 	$(MAKE) -C ocp-jslib
 	$(MAKE) -C ocaml-num
 	$(MAKE) -C toplevel
+	$(MAKE) -C toplevel-logic
 	$(MAKE) -C tutorial
 	$(MAKE) -C try-ocaml
+	$(MAKE) -C try-logic
 	$(MAKE) -C try-js_of_ocaml
 
 clean:
 	$(MAKE) -C js_of_ocaml clean
 	$(MAKE) -C ocp-jslib clean
 	$(MAKE) -C toplevel clean
+	$(MAKE) -C toplevel-logic clean
 	$(MAKE) -C ocaml-num clean
 	$(MAKE) -C tutorial clean
 	$(MAKE) -C try-ocaml clean
+	$(MAKE) -C try-logic clean	
 	$(MAKE) -C try-js_of_ocaml clean
 
 
@@ -24,9 +28,11 @@ depend:
 	$(MAKE) -C cmicomp depend
 	$(MAKE) -C ocp-jslib depend
 	$(MAKE) -C toplevel depend
+	$(MAKE) -C toplevel-logic depend
 	$(MAKE) -C ocaml-num depend
 	$(MAKE) -C tutorial depend
 	$(MAKE) -C try-ocaml depend
+	$(MAKE) -C try-logic depend	
 	$(MAKE) -C try-js_of_ocaml depend
 
 update-lessons:
---
+++ logic-lessons/lang/fr/messages.ml
@@ -0,0 +1,34 @@
+"Try Logic", "Logique pas à pas";
+"English", "Français";
+"The subjects of Logic with the notions of Formal Semantics and Theorem Proving, have a fundamental role for the basis of professionals related to fields such as computer science, mathematics, statistics and Philosophy. The Logic is related to the fundations of computer science, such as the calculus is related to the engineering. ",
+"Les notions de Sémantique Formelle et de démonstration de théorèmes, associée à la matière de Logique ont un role essenciel pour la formation de professionnel d'informatiques, mathematiques, estatistique et la philosophie, entre autres champs de recherche et atuations.";
+"Click here to execute this code", "Cliquer pour exécuter le code";
+
+  "Commands", "Commandes";
+  "Effects", "Actions";
+  "Enter / Return", "Entrée / Retour";
+  "Submit code", "Exécuter le code";
+  "Up / Down", "Haut / Bas";
+  "Cycle through history", "Historique";
+  "Shift + Enter", "Shift + Entrée";
+  "Multiline edition", "Édition Multiligne";
+  "Move to lesson 1", "Aller à la leçon 1";
+  "Move to step 1 of the current lesson", "Aller à l'étape 1 de la leçon courante";
+  "See available lessons", "Voir les leçons disponibles";
+  "See available steps in the current lesson", "Voir les étapes disponibles dans la leçon courante";
+  "Move to the next step", "Passer à l'étape suivante";
+  "Move to the previous step", "Revenir à l'étape précédente";
+
+  "You moved to lesson", "Vous êtes passé à la leçon";
+  "step", "étape";
+  "Congratulations", "Bravo";
+  "You moved to the next lesson", "Vous êtes passé à la leçon suivante";
+  "You moved to the next step", "Vous êtes passé à l'étape suivante";
+  "All steps in lesson", "Toutes les étapes de la leçon";
+  "All lessons", "Toutes les leçons";
+  "Send", "Exécuter";
+  "Clear", "Effacer";
+  "Reset", "Ré-init";
+  "Save", "Télécharger";
+  "Lesson", "Leçon";
+  "Step", "Étape";
