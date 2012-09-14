4e2e0e9da1e78042b82874b3ead1360c0a5be3a1
terrematte <git@github.com:terrematte/Logicamente.git>
2012-09-13 21:02:04 -0300

    More steps 6-9 of lesson 8.

--- logic-lessons/lang/tr/messages.ml
+++
@@ -1,8 +0,0 @@
-"English", "Turkçe";
-
-(* The rest needs to be done... *)
-"Try Logic", "Try Logic";
-"The subjects of Logic with the notions of Formal Semantics and Theorem Proving, have a fundamental role for the basis of professionals related to fields such as computer science, mathematics, statistics and Philosophy. The Logic is related to the fundations of computer science, such as the calculus is related to the engineering.",
-"The subjects of Logic with the notions of Formal Semantics and Theorem Proving, have a fundamental role for the basis of professionals related to fields such as computer science, mathematics, statistics and Philosophy. The Logic is related to the fundations of computer science, such as the calculus is related to the engineering.";
-"Click here to execute this code",
-"Click here to execute this code";
